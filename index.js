4aeed9080e8710af55dd340d89e60a5b6d214ff5
Julz LeU <julianleunes22@yahoo.com>
2021-10-28 16:43:05 -0500

    completedassignment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
-
-module.exports = {name, height, message};
+describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(message).toInclude(name);
+      expect(message).toInclude(height);
+    });
+  });
